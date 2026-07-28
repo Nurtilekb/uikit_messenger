ac487598756730ce69e5b3b45ed504aab6969824
Nurtilek <abdlhtv06@gmail.com>
2026-07-28 11:34:20 +0300

    Divide appbar from scaffold to another widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -23,8 +23,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
   List<Chat> chats = [
     Chat(
-      name: 'Nurtilek',
-      lastMessage: 'bro go to afsasdb asfdg wert werthy retqwer gtrewgwt r wet',
+      name: 'Kutman Sayitkanov',
+      lastMessage: 'bro, go to gym today?',
       time: '22 июня',
       avatar: '',
       unreadCount: 3,
@@ -47,16 +47,16 @@ class _HomeScreenState extends State<HomeScreen> {
       isOnline: true,
     ),
     Chat(
-      name: 'Daniyar',
-      lastMessage: 'Ок, договорились 👍',
+      name: 'Daniyar Ermatov',
+      lastMessage: 'Мы можем с вами встретиться?',
       time: '12:30',
       avatar: '',
       unreadCount: 1,
       isOnline: false,
     ),
     Chat(
-      name: 'Aizhan',
-      lastMessage: 'С днем рождения! 🎉',
+      name: 'Aizhan Matraimova',
+      lastMessage: 'Давай сегодя пойдем в ынтымак за ручки держась',
       time: '11:45',
       avatar: '',
       unreadCount: 0,
@@ -66,8 +66,7 @@ class _HomeScreenState extends State<HomeScreen> {
       name: 'Ruslan',
       lastMessage: 'Где ты? Я уже на месте',
       time: '10:00',
-      avatar:
-          'https://media.gq-magazine.co.uk/photos/5d1392adb363fa622820c7ec/1:1/w_1280,h_1280,c_limit/Conor-McGregor-GQ-20Dec16_rex_b.jpg',
+      avatar: '',
       unreadCount: 2,
       isOnline: false,
     ),
@@ -77,84 +76,46 @@ class _HomeScreenState extends State<HomeScreen> {
     final colors = context.appColors;
     final themeStyle = Theme.of(context);
     return Scaffold(
-      body: Stack(
-        children: [
-          SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(24, 14, 24, 10),
-                  child: Row(
-                    children: [
-                      Text(
-                        "chats".tr(),
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.w800,
-                          letterSpacing: -0.4,
-                          color: colors.textPrimary,
-                        ),
-                      ),
-                      const Spacer(),
-                      CircleIconButton(
-                        childd: Icon(
-                          Icons.search,
-                          size: 27,
-                          color: colors.textPrimary,
-                        ),
-                        onTap: () {
-                          context.router.push(const SearchRoute());
-                        },
-                      ),
-                      const SizedBox(width: 12),
-                      CircleIconButton(
-                        childd: Text(
-                          "me".tr(),
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                        background: themeStyle.dividerColor,
-                        onTap: () {
-                          context.router.push(ProfileRoute(name: 'nurtilek'));
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 30),
-                if (chats.isNotEmpty)
-                  Expanded(
-                    child: ListView.separated(
-                      padding: const EdgeInsets.fromLTRB(24, 2, 24, 100),
-                      itemCount: chats.length,
-                      separatorBuilder: (_, _) => const SizedBox(height: 25),
-                      itemBuilder: (context, index) {
-                        final chat = chats[index];
-                        return UserTile(
-                          onTap: () {
-                            context.router.push(
-                              ChatsRoute(
-                                numName: chat.name,
-                                isOnline: chat.isOnline,
-                                imageAvatar: chat.avatar,
-                              ),
-                            );
-                          },
-                          name: chat.name,
-                          lastMessage: chat.lastMessage,
-                          isOnline: chat.isOnline,
-                          unreadCount: chat.unreadCount,
-                          avatarUrl: chat.avatar,
-                          time: chat.time,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            HomeAppBar(
+              onTapProfile: () =>
+                  context.pushRoute(ProfileRoute(name: 'nurtilek')),
+              onTapSearch: () => context.pushRoute(const SearchRoute()),
+            ),
+            const SizedBox(height: 30),
+            if (chats.isNotEmpty)
+              Expanded(
+                child: ListView.separated(
+                  itemCount: chats.length,
+                  separatorBuilder: (_, _) => const SizedBox(height: 25),
+                  itemBuilder: (context, index) {
+                    final chat = chats[index];
+                    return UserTile(
+                      onTap: () {
+                        context.router.push(
+                          ChatsRoute(
+                            numName: chat.name,
+                            isOnline: chat.isOnline,
+                            imageAvatar: chat.avatar,
+                          ),
                         );
                       },
-                    ),
-                  ),
-                if (chats.isEmpty) Center(child: EmptyChatWidget()),
-              ],
-            ),
-          ),
-        ],
+                      name: chat.name,
+                      lastMessage: chat.lastMessage,
+                      isOnline: chat.isOnline,
+                      unreadCount: chat.unreadCount,
+                      avatarUrl: chat.avatar,
+                      time: chat.time,
+                    );
+                  },
+                ),
+              ),
+            if (chats.isEmpty) Center(child: EmptyChatWidget()),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
@@ -164,6 +125,47 @@ class _HomeScreenState extends State<HomeScreen> {
         },
         child: Icon(Icons.add, color: colors.textOnPrimary),
       ),
+    );
+  }
+}
+
+class HomeAppBar extends StatelessWidget {
+  final void Function()? onTapSearch;
+  final VoidCallback? onTapProfile;
+  const HomeAppBar({this.onTapSearch, this.onTapProfile, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = context.appColors;
+    final themeStyle = Theme.of(context);
+
+    return AppBar(
+      backgroundColor: colors.cardBackground,
+      actionsPadding: const EdgeInsets.fromLTRB(0, 8, 16, 8),
+      title: Text(
+        "chats".tr(),
+        style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.w800,
+          letterSpacing: -0.4,
+          color: colors.textPrimary,
+        ),
+      ),
+      actions: [
+        CircleIconButton(
+          childd: Icon(Icons.search, size: 27, color: colors.textPrimary),
+          onTap: onTapSearch,
+        ),
+        const SizedBox(width: 12),
+        CircleIconButton(
+          childd: Text(
+            "me".tr(),
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          background: themeStyle.dividerColor,
+          onTap: onTapProfile,
+        ),
+      ],
     );
   }
 }
