cea77485d00cc48ba8acdb0e01ab6a4a3d09f693
Nurtilek <abdlhtv06@gmail.com>
2026-07-30 15:26:41 +0300

    here checked users state and moving into inside

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,8 +1,44 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:uikit/router/app_router.dart';
 import 'package:uikit/theme/app_colors.dart';
 
-class SplashScreen extends StatelessWidget {
+@RoutePage()
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _navigateToNext();
+    });
+  }
+
+  void _navigateToNext() {
+    Future.delayed(const Duration(seconds: 2), () {
+      if (!mounted) return;
+      try {
+        final user = FirebaseAuth.instance.currentUser;
+        if (user != null) {
+          context.router.replaceAll([const HomeRoute()]);
+        } else {
+          context.router.replaceAll([const AuthRoute()]);
+        }
+      } catch (e) {
+        if (mounted) {
+          context.router.replaceAll([const AuthRoute()]);
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
